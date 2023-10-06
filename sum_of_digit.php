2c359af0b5f2fced390b818f0c6739c6431c7fa4
Vikash Kumar <abhi.vik.0542@gmail.com>
2023-10-06 15:22:51 +0530

    Vik commit

--- sum_of_digit.php
+++ sum_of_digit.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <style>
         body{
             padding:20px;
@@ -19,14 +16,18 @@
 <body>
     <h1>Input</h1>
     <div class="div">
-        <form method="post" action="front.php" >
+        <form id="form">
             <input class = "form-control mt-2 mb-2" type="number" name="number" id="">
             <input class= "btn btn-primary" type="submit" value="submit" name ="submit">
         </form>
     </div>
-     
-    
 
+    <script type="text/javascript">
+        $(document).ready(function() {
+            alert("Hello");
+        });
+
+    </script>
 
 </body>
 </html>
